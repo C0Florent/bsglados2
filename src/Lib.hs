92e191910cf3aa3fe0b11f8aeff15acedbfdedcf
Florent Charpentier <florent.charpentier@epitech.eu>
2024-12-11 15:25:26 +0100

    :sparkles: Add error handling to VM

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,8 @@ module Lib (
     Value(..),
     Operation(..),
     Instruction(..),
+    Insts,
+    Stack,
     exec
 ) where
 
@@ -24,16 +26,17 @@ data Instruction
 type Stack = [Value]
 type Insts = [Instruction]
 
-applyOp :: Operation -> Stack -> Stack
-applyOp Addition ((Number op1):(Number op2):stack) = Number (op1 + op2):stack
-applyOp Subtraction ((Number op1):(Number op2):stack) = Number (op1 - op2):stack
-applyOp Multiplication ((Number op1):(Number op2):stack) = Number (op1 * op2):stack
-applyOp Division ((Number op1):(Number op2):stack) = Number (op1 `div` op2):stack
-applyOp _ _ = error "Invalid operation"
+applyOp :: Operation -> Stack -> Either String Stack
+applyOp Addition ((Number op1):(Number op2):stack) = Right $ Number (op1 + op2):stack
+applyOp Subtraction ((Number op1):(Number op2):stack) = Right $ Number (op1 - op2):stack
+applyOp Multiplication ((Number op1):(Number op2):stack) = Right $ Number (op1 * op2):stack
+applyOp Division ((Number op1):(Number op2):stack) = Right $ Number (op1 `div` op2):stack
+applyOp Division (_:(Number 0):_) = Left "Division by zero"
+applyOp op _ = Left $ "Invalid arguments to " ++ show op
 
-exec :: Insts -> Stack -> Value
-exec [] _ = error "Missing return instruction"
-exec (Ret:_) [] = error "No value to return"
-exec (Ret:_) (val:_) = val
+exec :: Insts -> Stack -> Either String Value
+exec [] _ = Left "Missing return instruction"
+exec (Ret:_) [] = Left "No value to return"
+exec (Ret:_) (val:_) = Right val
 exec ((Push val):insts) stack = exec insts (val:stack)
-exec ((Call op):insts) stack = exec insts (applyOp op stack)
+exec ((Call op):insts) stack = applyOp op stack >>= exec insts
