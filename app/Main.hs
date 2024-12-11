f8291a9991093fbf3a14dcf52d306181db353557
Florent Charpentier <florent.charpentier@epitech.eu>
2024-12-11 15:25:14 +0100

    :recycle: Refactor main to better display tests

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,31 @@
-import Lib (exec, Instruction (Push, Ret, Call), Value (Number), Operation (Subtraction))
+import Lib (exec,
+    Instruction (Push,
+    Ret,
+    Call),
+    Value (Number),
+    Operation (Subtraction),
+    Insts,
+    )
+
+runCases :: [Insts] -> IO ()
+runCases cases = do
+    mapM_ (\insts -> do
+        putStr "Running: "
+        print insts
+        putStr "Result:"
+        print $ exec insts []
+        putStrLn "==="
+        ) cases
+    putStrLn "end of tests"
+
 main :: IO ()
 main = do
-    print $ exec [Push (Number 42), Ret] []
     putStrLn ""
-    print $ exec
-        [ Push (Number 10)
-        , Push (Number 52)
-        , Call Subtraction
-        , Ret
+    runCases
+        [ [Push (Number 42), Ret]
+        , [ Push (Number 10)
+          , Push (Number 52)
+          , Call Subtraction
+          , Ret
         ]
-        []
+        ]
